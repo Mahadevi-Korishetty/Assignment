340015c48371da854b5f26f0ec46c2543ef466a5
Mahadevi-Korishetty <korishettymahadevi@gmail.com>
2021-03-22 07:18:58 +0530

    trying to squash

--- main.go
+++ main.go
@@ -6,5 +6,6 @@ func main() {
 	fmt.Printf("Git repo")
 	fmt.Printf("Local commit-1")
 	fmt.Printf("Local commit-2")
-	fmt.Printf("Local commit-2")
+	fmt.Printf("Local commit-3")
+	fmt.Printf("Local commit-4")
 }
